dc841ff033fd72daa9d3fdf5bcd1028cd20de2f5
Đặng Đức Tin <ductindang1009@gmail.com>
2024-05-11 19:32:38 +0700

    puuuu

--- springweb2/src/main/java/Springweb/repository/thongtinsdRepository.java
+++ springweb2/src/main/java/Springweb/repository/thongtinsdRepository.java
@@ -1,6 +1,9 @@
 package Springweb.repository;
 
 import Springweb.entity.thongtinsd;
+import java.sql.Timestamp;
+
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.Query;
@@ -12,6 +15,12 @@ public interface thongtinsdRepository extends CrudRepository<thongtinsd, Integer
 //    @Query("SELECT t FROM thietbi t WHERE t.TenTB LIKE %:keyword%")
 //    List<thongtinsd> searchTTSD(String keyword);
 
-    @Query("SELECT s FROM thongtinsd s JOIN thietbi t ON s.MaTB = t.MaTB WHERE s.MaTB = :maTB")
-    List<thongtinsd> searchTTSD(int maTB);
+//    @Query("SELECT s FROM thongtinsd s JOIN thietbi t ON s.MaTB = t.MaTB WHERE s.MaTB = :maTB")
+//    List<thongtinsd> searchTTSD(int maTB);
+    
+//    @Query("SELECT s FROM thongtinsd s WHERE s.MaTB = :maTB")
+//    List<thongtinsd> findByMaTB(int maTB);
+    
+    @Query("SELECT s FROM thongtinsd s WHERE s.MaTB = :maTB AND s.TGMuon >= :startTime")
+    List<thongtinsd> findByMaTB(int maTB, Timestamp startTime);
 }
